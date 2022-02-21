e00fdd5b9e6f1745a49eb7841bec6d5960e55401
Cindy Cheung <cindycheung1408@gmail.com>
2022-02-21 20:43:50 +0000

    string ends with ?

---
+++ stringEndsWith/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(solution("", ""))
+	fmt.Println(solution(" ", ""))
+	fmt.Println(solution("abc", "c"))
+	fmt.Println(solution("banana", "ana"))
+	fmt.Println(solution("a", "z"))
+	fmt.Println(solution("", "t"))
+	fmt.Println(solution("$a = $b + 1", "+1"))
+	fmt.Println(solution("    ", "   "))
+	fmt.Println(solution("1oo", "100"))
+}
+
+func solution(str, ending string) bool {
+	strArr := strings.Split(str, "")
+	endingArr := strings.Split(ending, "")
+	matchEndingArr := []string{}
+	if len(strArr) == 0 && len(endingArr) != 0 {
+		return false
+	}
+	for i := 0; i < len(endingArr); i++ {
+		matchEndingArr = append(matchEndingArr, strArr[len(strArr)-1-i])
+	}
+	if strings.Join(matchEndingArr, "") == ending {
+		return true
+	}
+	return false
+}

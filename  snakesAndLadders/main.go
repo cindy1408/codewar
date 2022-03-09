9ea9a8fd74b71c45bae20dc30189eb36292ab230
cindy1408 <cindycheung1408@gmail.com>
2022-03-09 12:24:29 +0000

    testing commit works

---
+++  snakesAndLadders/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+
+// SnakesLadders - Stucture that game is played on
+type SnakesLadders struct {
+	// Define Game Struct
+}
+
+// NewGame - Method that starts a new game for the SnakesLadders struct
+func (sl *SnakesLadders) NewGame() {
+	// your code here
+}
+
+// Play - Method that makes turn givem a doce roll from die1 and die2 for the SnakesLadders struct
+// Player that dice is for should automatically be determined based on rules
+func (sl *SnakesLadders) Play(die1, die2 int) string {
+	// your code here
+}

dafc0df6e0e7fb077feb41dbc484085728e0fb6f
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2022-03-14 19:13:09 +0000

    rust: traffic lights

---
+++ rust/thinkful-LogicDrills:TrafficLight/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("{}", update_light("green")); //yellow
+    println!("{}", update_light("yellow")); //red
+    println!("{}", update_light("red")); //green
+}
+
+fn update_light(current: &str) -> String {
+    match current {
+        "red" => return "green".to_string(),
+        "yellow" => return "red".to_string(),
+        "green" => return "yellow".to_string(),
+        x => panic!("Unexpected invalid current {:?}", x),
+    }
+}
